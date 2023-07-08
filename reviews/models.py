8f2ef08c80d05ad518760ebf89595f950ef1385b
Suraj Wate <suraj.wate@gmail.com>
2023-07-08 13:21:29 +0100

    storing form data in a database

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=25)
+    review_text = models.CharField(max_length=200)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user_name} rated {self.rating} stars"
