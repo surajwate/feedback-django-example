32badeda9e9093713656392e61b2f0a3fd5269f4
Suraj Wate <suraj.wate@gmail.com>
2023-07-10 15:11:02 +0100

    DetailView

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .forms import ReviewForm
 from .models import Review
@@ -48,12 +48,6 @@ class ReviewListView(ListView):
     #     return data
     
     
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context
+    model = Review
